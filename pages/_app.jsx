18863ad1ccebc3c3fef177f4556942758d1984bc
ZinouWalid <zineddinewalid2021@gmail.com>
2022-05-02 11:15:19 +0100

    done 02_05_2022--01--

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,18 +4,15 @@ import { StateProvider } from '../React-Context-Api/context'
 import reducer, { initialState } from '../React-Context-Api/reducer'
 import '../public/Welcome.css'
 import { SessionProvider, useSession, signIn } from 'next-auth/react'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       {Component.auth ? (
-        <Auth>
           <StateProvider initialState={initialState} reducer={reducer}>
             <Component {...pageProps} />
           </StateProvider>
-        </Auth>
       ) : (
         <StateProvider initialState={initialState} reducer={reducer}>
          <Component {...pageProps} />
@@ -27,21 +24,3 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 }
 
 export default MyApp
-
-function Auth({ children }) {
-  const router = useRouter()
-  const { data: session, status } = useSession()
-  const isUser = session?.user
-  useEffect(() => {
-    if (status === 'loading') return
-    if (!isUser) signIn()
-  }, [isUser, status])
-
-  if (isUser) {
-    return children
-  }
-
-  // Session is being fetched, or no user.
-  // If no user, useEffect() will redirect.
-  return <div>Loading...</div>
-}
